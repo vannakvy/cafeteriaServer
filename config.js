e43b179009b9bb30f030061b9a48bd5249040d96
vannakvy <vannakvy2020@gmail.com>
2022-01-04 14:23:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 export var admin = require("firebase-admin");
+//for connection from nodejs to firebase 
 var serviceAccountCafe = require("./goglobal2021-2775b-firebase-adminsdk-jvl1v-6c57893bb3.json");
 
 admin.initializeApp({
